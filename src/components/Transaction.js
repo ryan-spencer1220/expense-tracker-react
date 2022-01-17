cb1e5ac4cea27248ef98de125dd2b84793a44007
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-01-16 20:18:14 -0800

    Add AddTransaction componet

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -8,7 +8,7 @@ export const Transaction = ({ transaction }) => {
 
   return (
     <li className={transaction.amount < 0 ? "minus" : "plus"}>
-      {transaction.text} <span>sign${transaction.amount}</span>
+      {transaction.text} <span>${transaction.amount}</span>
       <button
         onClick={() => deleteTransaction(transaction.id)}
         class="delete-btn"
